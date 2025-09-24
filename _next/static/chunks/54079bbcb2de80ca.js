eb2e0a2fe3e0d9a5bc5955ae9097120bf3d0066c
NoahC <128613517+Kallems23@users.noreply.github.com>
2025-09-24 16:14:25 +0200

    Updates

---
+++ _next/static/chunks/54079bbcb2de80ca.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/47f71f1e99830af6.js",
+  "static/chunks/turbopack-659294adb60a135e.js"
+])
---
+++ _next/static/chunks/bf5094417917a8d2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/47f71f1e99830af6.js",
+  "static/chunks/turbopack-42686ded984e2ae6.js"
+])
